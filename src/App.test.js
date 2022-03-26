cb1c87d149b57200a12248472467171c50fe3d7c
adamhinton <adam.d.hinton@gmail.com>
2022-03-25 21:46:58 -0400

    addes sanity checks to App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,17 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("Renders without errors", () => {
+test("[1] Sanity checks", () => {
+  const twoPlusTwo = 2 + 2;
+  expect(twoPlusTwo).toBe(4);
+  expect(twoPlusTwo).not.toBe(5);
+});
+
+test("[2] Renders without errors", () => {
   render(<App />);
 });
 
-test("renders learn react link", () => {
+test("[3] Renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/Hello World/i);
   expect(linkElement).toBeInTheDocument();
