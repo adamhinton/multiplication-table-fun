05e06329baf7f8f89f5c4ccc2dd40ca465969067
adamhinton <adam.d.hinton@gmail.com>
2022-03-25 21:45:32 -0400

    added "renders without errors" to App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,10 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
+test("Renders without errors", () => {
+  render(<App />);
+});
+
 test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/Hello World/i);
