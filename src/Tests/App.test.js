9c95113a00b065c18855da4d30d4029cf80e8758
adamhinton <adam.d.hinton@gmail.com>
2022-03-27 18:33:45 -0400

    Moved App.test.js in to new Tests folder

---
+++ src/Tests/App.test.js
@@ -0,0 +1,18 @@
+import { render, screen } from "@testing-library/react";
+import App from "../App";
+
+test("[1] Sanity checks", () => {
+  const twoPlusTwo = 2 + 2;
+  expect(twoPlusTwo).toBe(4);
+  expect(twoPlusTwo).not.toBe(5);
+});
+
+test("[2] Renders without errors", () => {
+  render(<App />);
+});
+
+test("[3] Renders learn react link", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Hello World/i);
+  expect(linkElement).toBeInTheDocument();
+});
