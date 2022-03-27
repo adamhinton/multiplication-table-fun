f6b8582627f454e064b806db2cdd819b30aef8f0
adamhinton <adam.d.hinton@gmail.com>
2022-03-27 18:11:47 -0400

    deleted everything except container div from App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello world!</h1>
-      </header>
-    </div>
-  );
+  return <div className="App">Hello World!</div>;
 }
 
 export default App;
