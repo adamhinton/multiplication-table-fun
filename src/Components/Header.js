38cf0e8c264fea32310ee83239e9ea1643702c16
adamhinton <adam.d.hinton@gmail.com>
2022-03-28 20:38:58 -0400

    created StyHeader and Styh1 components

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,20 @@
+import styled from "styled-components";
+
 const Header = (props) => {
   return (
-    <header>
-      <h1>Multiplication Table Fun</h1>
-    </header>
+    <StyHeader>
+      <StyH1>Multiplication Table Fun</StyH1>
+    </StyHeader>
   );
 };
 
 export default Header;
+
+const StyHeader = styled.header`
+  background-color: rgb(46, 37, 37);
+  color: rgb(221, 203, 203);
+`;
+
+const StyH1 = styled.h1`
+  width: 15%;
+`;
