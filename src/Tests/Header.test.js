a83a43f483f763e207d664befed3e8bd26834003
adamhinton <adam.d.hinton@gmail.com>
2022-03-29 23:36:27 -0400

    updated test to add dark mode button

--- src/Tests/Header.test.js
+++ src/Tests/Header.test.js
@@ -5,12 +5,14 @@ test("[1] Renders without errors", () => {
   render(<Header />);
 });
 
-test("[2] h1 and h2 displays properly", () => {
+test("[2] h1, h2 and dark mode button display properly", () => {
   render(<Header />);
 
   const h1 = screen.getByText(/multiplication table fun/i);
   const h2 = screen.getByText(/author: adam hinton/i);
+  const darkBtn = screen.getByText(/dark mode/);
 
   expect(h1).toBeInTheDocument();
   expect(h2).toBeInTheDocument();
+  expect(darkBtn).toBeInTheDocument;
 });
