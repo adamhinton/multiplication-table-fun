b7b144bd03d3c87e3b9b3e809fa44ed1fe4c5423
adamhinton <adam.d.hinton@gmail.com>
2022-04-01 15:28:56 -0400

    changed each table item to an html article, not sure if that's right

--- src/Components/SingleTableItem.js
+++ src/Components/SingleTableItem.js
@@ -2,11 +2,11 @@ import styled from "styled-components";
 
 const SingleTableItem = (props) => {
   return (
-    <section>
+    <article>
       <div> 1 x 1 = 1</div>
       <div> 1 x 2 = 2</div>
       <div> 1 x 3 = 3</div>
-    </section>
+    </article>
   );
 };
 
