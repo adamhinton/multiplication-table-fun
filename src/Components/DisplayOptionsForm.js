02dc1abec9868f9e8f8c3d75a65b30ed6865f7a9
adamhinton <adam.d.hinton@gmail.com>
2022-03-31 22:57:03 -0400

    inputted label and input tags

--- src/Components/DisplayOptionsForm.js
+++ src/Components/DisplayOptionsForm.js
@@ -1,7 +1,12 @@
 import styled from "styled-components";
 
 const DisplayOptionsForm = (props) => {
-  return <form></form>;
+  return (
+    <form>
+      <label>Multiplier:</label>
+      <input type="text"></input>
+    </form>
+  );
 };
 
 export default DisplayOptionsForm;
