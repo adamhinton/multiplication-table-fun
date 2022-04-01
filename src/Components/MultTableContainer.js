25fbd04ef9d05ab43f2696768639ccfee2acb6df
adamhinton <adam.d.hinton@gmail.com>
2022-04-01 15:26:15 -0400

    imported SingleTableItem to MultTableContainer

--- src/Components/MultTableContainer.js
+++ src/Components/MultTableContainer.js
@@ -1,11 +1,10 @@
 import styled from "styled-components";
+import SingleTableItem from "./SingleTableItem";
 
 const MultTableContainer = (props) => {
   return (
     <section>
-      <div> 1 x 1 = 1</div>
-      <div> 1 x 2 = 2</div>
-      <div> 1 x 3 = 3</div>
+      <SingleTableItem />
     </section>
   );
 };
