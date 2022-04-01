2d45569d7d0048cc6726814d081107c242821123
adamhinton <adam.d.hinton@gmail.com>
2022-04-01 15:22:35 -0400

    created MultTableContainer component with very basic single table

---
+++ src/Components/MultTableContainer.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const MultTableContainer = (props) => {
+  return (
+    <section>
+      <div> 1 x 1 = 1</div>
+      <div> 1 x 2 = 2</div>
+      <div> 1 x 3 = 3</div>
+    </section>
+  );
+};
