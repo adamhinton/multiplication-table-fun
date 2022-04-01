c60de581f6ed19fac0298cc2da39c684c0837d79
adamhinton <adam.d.hinton@gmail.com>
2022-03-31 22:44:51 -0400

    made DisplayOptionsForm.js for filling out display options; imported it to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from "./Components/Header";
+import DisplayOptionsForm from "./Components/DisplayOptionsForm";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <DisplayOptionsForm />
     </div>
   );
 }
---
+++ src/Components/DisplayOptionsForm.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+const DisplayOptionsForm = (props) => {
+  return <div>Blah Blah Blah</div>;
+};
+
+export default DisplayOptionsForm;
