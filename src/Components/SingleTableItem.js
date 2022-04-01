6e8f771654620c183e5450f2473af8e1bb636f46
adamhinton <adam.d.hinton@gmail.com>
2022-04-01 15:26:01 -0400

    created SingleTableItem.js for each individual table item

---
+++ src/Components/SingleTableItem.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const SingleTableItem = (props) => {
+  return (
+    <section>
+      <div> 1 x 1 = 1</div>
+      <div> 1 x 2 = 2</div>
+      <div> 1 x 3 = 3</div>
+    </section>
+  );
+};
+
+export default SingleTableItem;
