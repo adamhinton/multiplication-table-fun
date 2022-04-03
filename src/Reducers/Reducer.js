8ca6502093418fae4e609ee5d7f01d81b2c8bab3
adamhinton <adam.d.hinton@gmail.com>
2022-04-02 22:21:28 -0400

    Left comments mapping out my plan for reducer

--- src/Reducers/Reducer.js
+++ src/Reducers/Reducer.js
@@ -2,3 +2,10 @@ import { createSlice, configureStore } from "@reduxjs/toolkit";
 
 //Pro tip: I think action.payload is the argument. Can do something like action.payload is the multiplier.
 //This seems helpful: https://redux.js.org/tutorials/fundamentals/part-3-state-actions-reducers
+
+//Items to include in state:
+//-Display Pokemon toggle
+//-Display fun fact toggle
+//-Multiplier
+//-Limit
+//-Dark Mode Preference
