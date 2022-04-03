36cb51d8e66cac951969d809fb6d2b3a52e13da0
adamhinton <adam.d.hinton@gmail.com>
2022-04-03 17:09:34 -0400

    wrapped <App/> in <Provider> tags in index.js

--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ import store from "./app/store";
 import { Provider } from "react-redux";
 import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 reportWebVitals();
