722bc1df61db72e3997af2a6078415d3eed810bc
adamhinton <adam.d.hinton@gmail.com>
2022-04-02 22:19:24 -0400

    more helpful comments

--- src/Reducers/Reducer.js
+++ src/Reducers/Reducer.js
@@ -1,3 +1,4 @@
 import { createSlice, configureStore } from "@reduxjs/toolkit";
 
 //Pro tip: I think action.payload is the argument. Can do something like action.payload is the multiplier.
+//This seems helpful: https://redux.js.org/tutorials/fundamentals/part-3-state-actions-reducers
