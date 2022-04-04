de0eab7d635dd32a9c4c31cb127ec05dd6355cc9
adamhinton <adam.d.hinton@gmail.com>
2022-04-04 00:39:04 -0400

    left helpful comments

--- src/Reducers/reducer.js
+++ src/Reducers/reducer.js
@@ -1,1 +1,3 @@
+//NOTE: The reason I am "combining reducers" here when I only have one reducer, is that this app will soon have at least two, probably more reducers. So I am planning ahead.
+
 import tableValuesReducer from "./tableValuesSlice";
