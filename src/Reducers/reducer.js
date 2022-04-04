f800e5e567280e7b1793018ae292ea959cd59620
adamhinton <adam.d.hinton@gmail.com>
2022-04-04 00:39:45 -0400

    import combineReducers

--- src/Reducers/reducer.js
+++ src/Reducers/reducer.js
@@ -1,3 +1,4 @@
 //NOTE: The reason I am "combining reducers" here when I only have one reducer, is that this app will soon have at least two, probably more reducers. So I am planning ahead.
 
+import { combineReducers } from "@reduxjs/toolkit";
 import tableValuesReducer from "./tableValuesSlice";
