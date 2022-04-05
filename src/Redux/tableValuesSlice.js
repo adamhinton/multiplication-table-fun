23db4aaf03724e9cd9571fcff74c992122517c38
adamhinton <adam.d.hinton@gmail.com>
2022-04-04 22:09:17 -0400

    del unneeded comments

--- src/Redux/tableValuesSlice.js
+++ src/Redux/tableValuesSlice.js
@@ -1,6 +1,3 @@
-// import { createSlice, configureStore } from "@reduxjs/toolkit";
-
-//Pro tip: I think action.payload is the argument. Can do something like action.payload is the multiplier.
 //This seems helpful: https://redux.js.org/tutorials/fundamentals/part-3-state-actions-reducers
 
 //Items to include in state:
@@ -15,27 +12,6 @@
 //Notes:
 //Actions are objects that have a type key and a payload key
 //actions should contain the smallest amount of information needed to describe what happened.
-
-//Splitting reducers:
-//Split in to different reducers in different files, organized by which part of the app they update. See the link above for more.
-
-//Example initial state: (will add more things in as I implement more functionality)
-//const initialState = {
-//     multiplier: 10,
-//     limit: 25,
-// }
-
-// list of actions just for my reference
-// const actionsForReferenceOnly = [
-//   {
-//     type: "NEWMULTIPLIERANDLIMIT",
-//     //payload is an array, first number is new multiplier, second is new limit
-//     payload: {
-//       multiplier: 22,
-//       limit: 35,
-//     },
-//   },
-// ];
 
 const tableValuesReducer = (state, action) => {
   switch (action.type) {
