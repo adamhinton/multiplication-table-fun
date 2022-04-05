105a0d8d1c3ad30350631cf863e26f8467aedc3e
adamhinton <adam.d.hinton@gmail.com>
2022-04-04 22:16:06 -0400

    assigned "NEWMULTIPLIERANDLIMIT" action type to a variable to avoid typos

--- src/Redux/tableValuesSlice.js
+++ src/Redux/tableValuesSlice.js
@@ -1,6 +1,8 @@
+const NEWMULTIPLIERANDLIMIT = "NEWMULTIPLIERANDLIMIT";
+
 const tableValuesReducer = (state, action) => {
   switch (action.type) {
-    case "NEWMULTIPLIERANDLIMIT": {
+    case NEWMULTIPLIERANDLIMIT: {
       //things might need to change here based on the keys in rootReducer object, not totally sure yet
       return {
         ...state,
