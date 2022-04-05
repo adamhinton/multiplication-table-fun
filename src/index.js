181b84af15874c903704647e5a15b16686823775
adamhinton <adam.d.hinton@gmail.com>
2022-04-04 21:30:05 -0400

    correct erroneous store import

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import store from "./app/store";
+import store from "./Redux/store";
+// import store from "./app/store";
 import { Provider } from "react-redux";
 import reportWebVitals from "./reportWebVitals";
-import 
 
 ReactDOM.render(
   <Provider store={store}>
