cfd5483d69e057465f70b32a22505ff15a9b8336
adamhinton <adam.d.hinton@gmail.com>
2022-04-09 22:38:26 -0400

    add black border to STI

--- src/Components/SingleTableItem.js
+++ src/Components/SingleTableItem.js
@@ -14,8 +14,9 @@ const SingleTableItem = (props) => {
 export default SingleTableItem;
 
 const StySingleTableItem = styled.article`
+  border: 1px solid black;
   background-color: #bef8f8;
-  margin: 0.25%;
+  /* margin: 0.25%; */
   color: black;
   padding: 2% auto;
   display: flex;
