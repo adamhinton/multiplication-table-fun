4362601c83f0ba490378d96cac2d2bffc385ddcb
adamhinton <adam.d.hinton@gmail.com>
2022-04-09 21:12:03 -0400

    import Provider and Store, and wrap DOF.test.js component in redux Provider tags so it tests properly

--- src/Tests/DisplayOptionsForm.test.js
+++ src/Tests/DisplayOptionsForm.test.js
@@ -1,14 +1,22 @@
 import { render, screen } from "@testing-library/react";
 import DisplayOptionsForm from "../Components/DisplayOptionsForm";
-import store from "./Redux/store";
+import store from "../Redux/store";
 import { Provider } from "react-redux";
 
 test.skip("[1] Renders without errors", () => {
-  render(<DisplayOptionsForm />);
+  render(
+    <Provider store={store}>
+      <DisplayOptionsForm />
+    </Provider>
+  );
 });
 
 test("[2] multiplier input and label appear in document", () => {
-  render(<DisplayOptionsForm />);
+  render(
+    <Provider store={store}>
+      <DisplayOptionsForm />
+    </Provider>
+  );
 
   const multiplierLabel = screen.getByTestId("multiplier-label");
   const multiplierInput = screen.getByTestId("multiplier-input");
@@ -18,7 +26,11 @@ test("[2] multiplier input and label appear in document", () => {
 });
 
 test.skip("[2] Display limit input and label appear in document", () => {
-  render(<DisplayOptionsForm />);
+  render(
+    <Provider store={store}>
+      <DisplayOptionsForm />
+    </Provider>
+  );
 
   const multiplierLabel = screen.getByTestId("display-limit-label");
   const multiplierInput = screen.getByTestId("display-limit-input");
@@ -28,7 +40,11 @@ test.skip("[2] Display limit input and label appear in document", () => {
 });
 
 test.skip("[2] Submit Button appears in document", () => {
-  render(<DisplayOptionsForm />);
+  render(
+    <Provider store={store}>
+      <DisplayOptionsForm />
+    </Provider>
+  );
 
   const submitBtn = screen.getByTestId("form-submit-button");
   expect(submitBtn).toBeInTheDocument();
