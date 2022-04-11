8792a6787d209a3a71c067b3a633453eb7bf1b4d
adamhinton <adam.d.hinton@gmail.com>
2022-04-11 12:50:11 -0400

    del unneeded code

--- src/Components/DisplayOptionsForm.js
+++ src/Components/DisplayOptionsForm.js
@@ -72,7 +72,6 @@ const StyForm = styled.form`
   background-color: #bef8f8;
   display: flex;
   flex-direction: column;
-  /* justify-content: center; */
   align-items: center;
   padding-top: 2%;
 `;
