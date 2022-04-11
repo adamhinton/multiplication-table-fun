ad273f9532d7b27da87caac954bc7ad2cd8089aa
adamhinton <adam.d.hinton@gmail.com>
2022-04-11 12:46:36 -0400

    center user preference form

--- src/Components/DisplayOptionsForm.js
+++ src/Components/DisplayOptionsForm.js
@@ -72,7 +72,8 @@ const StyForm = styled.form`
   background-color: #bef8f8;
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  /* justify-content: center; */
+  align-items: center;
   padding-top: 2%;
 `;
 
