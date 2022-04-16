7c578d108eae6dc5f7a91829c23c3187bae9e909
adamhinton <adam.d.hinton@gmail.com>
2022-04-15 21:43:23 -0400

    helpful comments in PDOS.js

--- src/Redux/pokemonDisplayOptionSlice.js
+++ src/Redux/pokemonDisplayOptionSlice.js
@@ -1,5 +1,8 @@
 const TOGGLEPOKEMONDISPLAYOPTION = "TOGGLEPOKEMONDISPLAYOPTION";
 
+//The user is able to choose whether to display a Pokemon for each number in the multiplication table
+//This dispatches their choice to the store when the user toggles the option in the form.
+
 const pokemonDisplayToggleReducer = (state, action) => {
   switch (action.type) {
     case TOGGLEPOKEMONDISPLAYOPTION: {
