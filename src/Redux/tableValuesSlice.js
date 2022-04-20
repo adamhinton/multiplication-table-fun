1c58374061d2df79126bc9bfe74f754eef2a47b5
adamhinton <adam.d.hinton@gmail.com>
2022-04-19 20:12:30 -0400

    del unneeded c.logs

--- src/Redux/tableValuesSlice.js
+++ src/Redux/tableValuesSlice.js
@@ -10,7 +10,7 @@ const NEWMULTIPLIERANDLIMIT = "NEWMULTIPLIERANDLIMIT";
 const tableValuesReducer = (state, action) => {
   switch (action.type) {
     case NEWMULTIPLIERANDLIMIT: {
-      console.log("action.payload:", action.payload);
+      // console.log("action.payload:", action.payload);
       //things might need to change here based on the keys in rootReducer object, not totally sure yet
       return {
         multiplier: action.payload.multiplier,
