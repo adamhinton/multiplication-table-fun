825d0e3bdba7c8a10e05461a90e7aef44ced3618
adamhinton <adam.d.hinton@gmail.com>
2022-04-21 20:38:20 -0400

    complete test thst STI.js renders without errors

--- src/Tests/SingleTableItem.test.js
+++ src/Tests/SingleTableItem.test.js
@@ -4,3 +4,11 @@ import configureStore from "../Redux/store";
 import { Provider } from "react-redux";
 
 const store = configureStore();
+
+test("[1] Renders without errors", () => {
+  render(
+    <Provider store={store}>
+      <SingleTableItem />
+    </Provider>
+  );
+});
