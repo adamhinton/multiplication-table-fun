7ba757d27fc5a7e497062a006dc1129ed7b59461
adamhinton <adam.d.hinton@gmail.com>
2022-04-21 20:25:19 -0400

    needed imports for MTC.test

---
+++ src/Tests/MultTableContainer.test.js
@@ -0,0 +1,6 @@
+import MultTableContainer from "../Components/MultTableContainer";
+import { render, screen } from "@testing-library/react";
+import configureStore from "../Redux/store";
+import { Provider } from "react-redux";
+
+const store = configureStore();
