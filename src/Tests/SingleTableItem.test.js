a1260dd138b2ad53028303a3613b6bf4842e0ec5
adamhinton <adam.d.hinton@gmail.com>
2022-04-21 20:37:49 -0400

    necessary imports for STI.test

---
+++ src/Tests/SingleTableItem.test.js
@@ -0,0 +1,6 @@
+import SingleTableItem from "../Components/SingleTableItem";
+import { render, screen } from "@testing-library/react";
+import configureStore from "../Redux/store";
+import { Provider } from "react-redux";
+
+const store = configureStore();
