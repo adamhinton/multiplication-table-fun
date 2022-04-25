fbcf1be72ce1383cef5a031e7eee237bf412f2f4
adamhinton <adam.d.hinton@gmail.com>
2022-04-24 20:48:23 -0400

    Successfully taught STI.js to only display poke img if isDisplayPokemon is true
    Next goal: Fix styling for both display options

--- src/Components/SingleTableItem.js
+++ src/Components/SingleTableItem.js
@@ -2,20 +2,22 @@ import styled from "styled-components";
 
 const SingleTableItem = (props) => {
   const { toBeMultiplied, currentMultiplier, isDisplayPokemon } = props;
-  console.log("isDisplayPokemon:", isDisplayPokemon);
+  // console.log("isDisplayPokemon:", isDisplayPokemon);
   const total = toBeMultiplied * currentMultiplier;
 
   return (
     <StySingleTableItem data-testid="single-table-item">
       {toBeMultiplied} x {currentMultiplier} = {total}
       {/* contains pokemon img if user has toggled that option */}
-      <StyPokemonFigure>
-        <img
-          data-testid="poke-img"
-          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${total}.png`}
-          alt="pokemon"
-        />
-      </StyPokemonFigure>
+      {isDisplayPokemon && (
+        <StyPokemonFigure>
+          <img
+            data-testid="poke-img"
+            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${total}.png`}
+            alt="pokemon"
+          />
+        </StyPokemonFigure>
+      )}
     </StySingleTableItem>
   );
 };
