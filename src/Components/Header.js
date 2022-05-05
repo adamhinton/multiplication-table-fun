e6b7d91ce2c632b21c9826fbdd03327d926ec124
adamhinton <adam.d.hinton@gmail.com>
2022-05-04 22:35:58 -0400

    import useState to header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import linksArray from "../utils/HeaderUtils/linksArray";
 import linkMaker from "../utils/HeaderUtils/linkMaker";
 import { BsMoon, BsSun } from "react-icons/bs";
+import { useState } from "react";
 
 const Header = (props) => {
   return (
