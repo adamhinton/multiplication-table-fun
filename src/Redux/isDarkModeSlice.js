2a594848f46f903af18dc9e071d99c7cbac37ab4
adamhinton <adam.d.hinton@gmail.com>
2022-05-10 19:26:03 -0400

    create darkmodeToggleReducer and export it

--- src/Redux/isDarkModeSlice.js
+++ src/Redux/isDarkModeSlice.js
@@ -1,1 +1,13 @@
 const TOGGLEDARKMODE = "TOGGLEDARKMODE";
+
+const darkModeToggleReducer = (state, action) => {
+  switch (action.type) {
+    case TOGGLEDARKMODE: {
+      return !state.isDarkMode;
+    }
+    default:
+      return false;
+  }
+};
+
+export default darkModeToggleReducer;
