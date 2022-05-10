2ff4ddd54db7386e8d1fc927052c27d791def84a
adamhinton <adam.d.hinton@gmail.com>
2022-05-10 18:29:00 -0400

    del unneeded code

--- src/Redux/reducer.js
+++ src/Redux/reducer.js
@@ -1,6 +1,3 @@
-//NOTE: The reason I am "combining reducers" here when I only have one reducer, is that this app will soon have at least two, probably more reducers. So I am planning ahead.
-//This combines all reducers in to one for modularity. Much easier than having a giant file since I will eventually have four or five reducers.
-
 // DARK MODE PLAN:
 // -create darkModeSlice.js with toggler action
 //-???create useDarkMode hook to get preference from localStorage????
