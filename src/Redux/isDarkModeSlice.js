bfcb3bbe1730b7d77123226100f37d1644467150
adamhinton <adam.d.hinton@gmail.com>
2022-05-15 14:38:55 -0400

    helpful comments in isDarkModeSlice.js

--- src/Redux/isDarkModeSlice.js
+++ src/Redux/isDarkModeSlice.js
@@ -1,3 +1,6 @@
+// When user hits dark mode button in header, this toggles dark mode/light mode, and sets preference in localStorage
+// On page load, this checks localStorage for darkMode preference
+
 import { TOGGLEDARKMODE } from "./actionNameVariables";
 
 const darkModeToggleReducer = (state, action) => {
