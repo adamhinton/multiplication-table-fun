e1f5493adbfcf87f96f8e056d55e8c9a698ae0aa
adamhinton <adam.d.hinton@gmail.com>
2022-05-15 14:36:00 -0400

    comment out plans in MTC.js

--- src/Components/MultTableContainer.js
+++ src/Components/MultTableContainer.js
@@ -1,18 +1,15 @@
 import styled from "styled-components";
 import SingleTableItem from "./SingleTableItem";
-//useSelector replaces a subscribe function; it knows when state updates and updates components if needed
 import { useSelector } from "react-redux";
 const selectState = (state) => state;
 
 const MultTableContainer = (props) => {
-  //pulls in global state from Redux store
   const state = useSelector(selectState);
   const { multiplier, limit } = state.tableValues;
   const isDisplayPokemon = state.displayPokemon.isDisplayPokemon;
 
   //creating an array to map over to generate multiplication table
-  //it's just full of zeroes, it could contain anything, doesn't matter. I just can't map over a sparse array,
-  //so I had to fill it with something.
+  // I had to fill this with something. I don't think this is the best method- refactor later
   const tableItemsArray = Array(limit).fill(0);
 
   return (
