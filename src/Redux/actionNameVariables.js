e2eaa56380b81764b9125f59c88a2085162f767a
adamhinton <adam.d.hinton@gmail.com>
2022-05-15 14:37:19 -0400

    better comments in actionNameVariables.js

--- src/Redux/actionNameVariables.js
+++ src/Redux/actionNameVariables.js
@@ -1,5 +1,5 @@
-// Store redux action names in one place
-//Import them any time they're used
+// Store redux action names in one place as a single source of truth
+// Import them any time they're used
 // Avoids errors from mistyping or changing these in one place but not another
 
 export const NEWMULTIPLIERANDLIMIT = "NEWMULTIPLIERANDLIMIT";
